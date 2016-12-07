82c75f8d97d537ebb009f0191a4eca9c6c8caee7
rak <emailrak@gmail.com>
2016-12-07 23:19:43 +0000

    wip

---
+++ reader/src/main/scala/com/paterake/spark/etl/reader/catalogue/DataCatalogueConfigReader.scala
@@ -0,0 +1,8 @@
+package com.paterake.spark.etl.reader.catalogue
+
+/**
+  * Created by rak on 07/12/2016.
+  */
+class DataCatalogueConfigReader {
+
+}
---
+++ reader/src/main/scala/com/paterake/spark/etl/reader/etl/EtlConfigReader.scala
@@ -0,0 +1,8 @@
+package com.paterake.spark.etl.reader.etl
+
+/**
+  * Created by rak on 07/12/2016.
+  */
+class EtlConfigReader {
+
+}
